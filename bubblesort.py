4a0c22595f3b9671d8584231c60dfabfaa0c9330
Владимир <vladimir-ratnikov2002@mail.ru>
2022-10-19 18:26:37 +0300

    First change of second member

--- bubblesort.py
+++ bubblesort.py
@@ -1,34 +1,25 @@
-# Python program for implementation of Bubble Sort
- 
-def bubbleSort(arr):
-    n = len(arr)
-    # optimize code, so if the array is already sorted, it doesn't need
-    # to go through the entire process
-    swapped = False
-    # Traverse through all array elements
-    for i in range(n-1):
-        # range(n) also work but outer loop will
-        # repeat one time more than needed.
-        # Last i elements are already in place
-        for j in range(0, n-i-1):
- 
-            # traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
-            if arr[j] > arr[j + 1]:
-                swapped = True
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-         
-        if not swapped:
-            # if we haven't needed to make a single swap, we
-            # can just exit the main loop.
-            return
- 
+# Insertion sort in Python
+
+
+def insertionSort(array):
+
+    for step in range(1, len(array)):
+        key = array[step]
+        j = step - 1
+        
+        # Compare key with each element on the left of it until an element smaller than it is found
+        # For descending order, change key<array[j] to key>array[j].        
+        while j >= 0 and key < array[j]:
+            array[j + 1] = array[j]
+            j = j - 1
+        
+        # Place key at after the element just smaller than it.
+        array[j + 1] = key
  
 # Driver code to test above
 arr = [64, 34, 25, 12, 22, 11, 90]
  
-bubbleSort(arr)
+insertionSort(arr)
  
 print("Sorted array is:")
 for i in range(len(arr)):
